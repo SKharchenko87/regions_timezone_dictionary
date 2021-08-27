7867260c891866829bbac8991f94889e4af548d9
Sergey.Kharchenko <skharchenko87@gmail.com>
2021-08-28 02:24:37 +0300

    Rename and reformat

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_csv_range_phone(src='web'):
     if src == 'web':
         url = "https://rossvyaz.gov.ru/about/otkrytoe-pravitelstvo/otkrytye-dannee/reestr-otkrytykh-dannykh"
         html_text = requests.get(url, verify=False).text
         soup = BeautifulSoup(html_text, 'lxml')
         url_csv_range_phone = soup.find('a', text='CSV-DEF-9').get('href')
-        arr = requests.get(url_csv_range_phone, verify=False).text
+        ds = requests.get(url_csv_range_phone, verify=False).text
     elif src == 'file':
         f = open("CSV-DEF-9.csv", "r")
-        arr = f.read()
+        ds = f.read()
     else:
-        []
-    arr = arr.split('\n')
-    arr.pop(0)
-    return list(map(lambda s: s.split(';'), arr))
+        ds = ""
+    res = ds.split('\n')
+    res.pop(0)
+    return list(map(lambda s: s.split(';'), res))
 
 
 if __name__ == '__main__':
